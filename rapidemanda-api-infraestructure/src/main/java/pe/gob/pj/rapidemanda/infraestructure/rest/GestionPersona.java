4a385c5cad7462a6d5877e70aa63f31f6c52e469
Oscar <dvillegas1207@gmail.com>
2025-09-15 15:02:10 -0500

    update Del

--- rapidemanda-api-infraestructure/src/main/java/pe/gob/pj/rapidemanda/infraestructure/rest/GestionPersona.java
+++ rapidemanda-api-infraestructure/src/main/java/pe/gob/pj/rapidemanda/infraestructure/rest/GestionPersona.java
@@ -7,7 +7,6 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
